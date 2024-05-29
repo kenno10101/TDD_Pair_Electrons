aeeab1c640200afed2d3c3f56b7be300cb2fb3fc
Kenn Sanga <kennsanga@gmail.com>
2024-05-29 15:38:11 +0200

    5 correct testcases in a row

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -16,4 +16,11 @@ describe('Electrons around the cores', function () {
 	it('should return 12', function () {
 		expect(game.count([3, 5, 3, 5, 4, 2])).toEqual(12);
 	})
+	it('5 test cases', function () {
+		expect(game.count([3, 1, 3, 5, 6, 2])).toEqual(8);
+		expect(game.count([6, 4, 2, 1, 3, 4])).toEqual(2);
+		expect(game.count([1, 4, 2, 1, 4])).toEqual(0);
+		expect(game.count([6, 6, 6, 6])).toEqual(0);
+		expect(game.count([4, 2, 3, 1, 2, 5])).toEqual(6);
+	})
 });
--- src/electrons.js
+++ src/electrons.js
@@ -1,5 +1,5 @@
 function Electrons() { }
-const values = [0, 0, 2, 0, 4, 6];
+const values = [0, 0, 2, 0, 4, 0];
 Electrons.prototype.count = function (dicerollArray) {
 	return dicerollArray.reduce((sum, v) => sum + values[v - 1], 0)
 };
