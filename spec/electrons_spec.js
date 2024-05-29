aaf06e75bb97cac0baf11744c2b3e8d627db06d8
Kenn Sanga <kennsanga@gmail.com>
2024-05-29 14:41:39 +0200

    todo 2: step 1

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -5,4 +5,8 @@ describe('Electrons around the cores', function () {
 	it('should return 0 for a string of 5 1s', function () {
 		expect(game.count([1, 1, 1, 1, 1])).toEqual(0);
 	});
+
+	it('should return 6', function () {
+		expect(game.count([1, 2, 3, 4, 5])).toEqual(6);
+	})
 });
