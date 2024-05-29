846dda2dcd13777f017ec8e01bd7cdb6307c0aff
Kenn Sanga <kennsanga@gmail.com>
2024-05-29 14:56:36 +0200

    todo 3: step 2

--- src/electrons.js
+++ src/electrons.js
@@ -1,6 +1,6 @@
 function Electrons() { }
 Electrons.prototype.count = function (dicerollArray) {
 	const arrayLength = dicerollArray.length;
-	const result = dicerollArray[arrayLength - 1] == 1 ? 0 : dicerollArray[arrayLength - 1] + 1;
+	const result = dicerollArray[arrayLength - 1] == 1 ? dicerollArray[0] == 1 ? 0 : 4 : dicerollArray[arrayLength - 1] + 1;
 	return result;
 };
